e2057c28a90952e59f4a75415595e4c57d85d74e
Josh Bach <bachjosh@gmail.com>
2023-11-14 18:54:25 -0600

    Setup styling, nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,8 @@
-import logo from '../assets/logo.svg';
 import Nav from './Nav';
 
 function Header() {
   return (
     <header>
-      <img src={logo} alt="Little Lemon logo" />
       <Nav />
     </header>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,26 +1,37 @@
+import { useState } from 'react';
+import logo from '../assets/logo.svg';
+import menuIcon from '../assets/icon _hamburger menu.svg'
+
 const links = [
-    {href: '', name: 'Home'},
-    {href: '', name: 'About'},
-    {href: '', name: 'Menu'},
-    {href: '', name: 'Reservations'},
-    {href: '', name: 'Order Online'},
-    {href: '', name: 'Login'},
-  ];
-  
-  function Nav() {
-    return (
-      <nav>
-        <ul>
-          {links.map((item) => 
-            <li>
-              <a href={item.href}>
-                {item.name}
-              </a>
-            </li>
-          )}
-        </ul>
-      </nav>
-    );
-  };
-  
-  export default Nav;
+  { href: '', name: 'Home' },
+  { href: '', name: 'About' },
+  { href: '', name: 'Menu' },
+  { href: '', name: 'Reservations' },
+  { href: '', name: 'Order Online' },
+  { href: '', name: 'Login' },
+];
+
+function Nav() {
+
+  const [isMenuExpanded, setIsMenuExpanded] = useState(false);
+
+  return (
+    <nav>
+      <img src={logo} alt="Little Lemon logo" />
+      <button onClick={() => setIsMenuExpanded(!isMenuExpanded)}>
+        <img src={menuIcon} alt="Menu expand icon" />
+      </button>
+      <ul className={isMenuExpanded ? 'expanded' : ''}>
+        {links.map((item) =>
+          <li>
+            <a href={item.href}>
+              {item.name}
+            </a>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
